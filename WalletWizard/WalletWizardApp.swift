22551a3c1cd7ce138bcbf9e9928332424203f97e
bhullaragam <asbhullar@ucdavis.edu>
2024-01-14 20:21:56 -0800

    Initial Commit

---
+++ WalletWizard/WalletWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletWizardApp.swift
+//  WalletWizard
+//
+//  Created by Agam Bhullar on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
